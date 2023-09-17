ebe963c2214d79e1d52f1400a82b28c107023544
gamedevengineer4489 <60021897+gamedevengineer4489@users.noreply.github.com>
2023-09-16 22:24:59 -0700

    Making a website that uses the News API

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const APP = express();
+const PORT = 3000;
+const API_KEY = 'ec8eb98887214d87a4c96c99458d926f';
+
+APP.use(express.static("public"));
+APP.use(bodyParser.urlencoded({  extended: true }))
+APP.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+APP.post("/", async (req, res) => {
+    try {
+        var response = await axios.get("https://newsapi.org/v2/everything?language=en&apiKey=" + API_KEY + "&q=" + req.body.search);
+        res.render("index.ejs", {data: response.data.articles, numberOfResults: response.data.totalResults });
+    } catch(error) {
+        res.render("index.ejs");
+    }
+    
+});
+
+APP.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
